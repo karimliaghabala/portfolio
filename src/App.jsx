622c744dac1f772b259514e1f6b66aa90532a083
karimliaghabala <agabala.93@gmail.com>
2023-01-11 12:07:31 +0400

    Update App.jsx
    update home page

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light nav-back ">
-        <Link className="navbar-brand" to="/about">Kərimli Ağabala</Link>
+        <Link className="navbar-brand" to="/">Kərimli Ağabala</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -36,6 +36,7 @@ export default function App() {
         </div>
       </nav>
       <Routes>
+        <Route path='/' element={<About />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/myworks' element={<Myworks />} />
