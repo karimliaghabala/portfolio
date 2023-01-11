b7ce9387f0185616bfae654e4009a3b2d2468da5
karimliaghabala <agabala.93@gmail.com>
2023-01-11 23:24:54 +0400

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,72 +10,7 @@ import Image6 from '../assets/image/image6.jpg'
 export default function Footer() {
   return (
     <>
-      <footer className="text-center text-white" style={{ backgroundColor: '#caced1' }}>
-        {/* Grid container */}
-        <div className="container p-4">
-          {/* Section: Images */}
-          <section >
-            <div className="row">
-              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
-                <div className="bg-image hover-overlay ripple shadow-1-strong rounded" data-ripple-color="light">
-                  <img src={Image1} className="w-100" />
-                  <Link to="#!">
-                    <div className="mask" style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }} />
-                  </Link>
-                </div>
-              </div>
-              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
-                <div className="bg-image hover-overlay ripple shadow-1-strong rounded" data-ripple-color="light">
-                  <img src={Image2} className="w-100" />
-                  <Link to="#!">
-                    <div className="mask" style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }} />
-                  </Link>
-                </div>
-              </div>
-              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
-                <div className="bg-image hover-overlay ripple shadow-1-strong rounded" data-ripple-color="light">
-                  <img src={Image3} className="w-100" />
-                  <Link to="#!">
-                    <div className="mask" style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }} />
-                  </Link>
-                </div>
-              </div>
-              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
-                <div className="bg-image hover-overlay ripple shadow-1-strong rounded" data-ripple-color="light">
-                  <img src={Image4} className="w-100" />
-                  <Link to="#!">
-                    <div className="mask" style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }} />
-                  </Link>
-                </div>
-              </div>
-              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
-                <div className="bg-image hover-overlay ripple shadow-1-strong rounded" data-ripple-color="light">
-                  <img src={Image5} className="w-100" />
-                  <Link to="#!">
-                    <div className="mask" style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }} />
-                  </Link>
-                </div>
-              </div>
-              <div className="col-lg-2 col-md-12 mb-4 mb-md-0">
-                <div className="bg-image hover-overlay ripple shadow-1-strong rounded" data-ripple-color="light">
-                  <img src={Image6} className="w-100" />
-                  <Link to="#!">
-                    <div className="mask" style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }} />
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </section>
-          {/* Section: Images */}
-        </div>
-        {/* Grid container */}
-        {/* Copyright */}
-        <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-          © 2023 Copyright:
-          <Link className="text-white" to="#!">Karimli Aghabala</Link>
-        </div>
-        {/* Copyright */}
-      </footer>
+     
     </>
   )
 }
