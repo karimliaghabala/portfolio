7f35e7264e438856e282af7e6cbaaf8c75bddc55
Karimli Aghabala <54455724+karimliaghabala@users.noreply.github.com>
2023-01-12 00:59:57 +0400

    Update sertficat.jsx
    Update

--- src/components/sertficat.jsx
+++ src/components/sertficat.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './sertficat.css'
-// import Image1 from '../assets/image/sertficats/1.jpeg'
-// import Image2 from '../assets/image/sertficats/2.jpg'
-// import Image3 from '../assets/image/sertficats/3.jpg'
-// import Image4 from '../assets/image/sertficats/4.jpg'
-// import Image5 from '../assets/image/sertficats/5.jpg'
-// import Image6 from '../assets/image/sertficats/6.jpg'
+ import Image1 from '../assets/image/sertficats/ser1.jpeg'
+ import Image2 from '../assets/image/sertficats/ser2.jpg'
+ import Image3 from '../assets/image/sertficats/ser3.jpg'
+ import Image4 from '../assets/image/sertficats/ser4.jpg'
+ import Image5 from '../assets/image/sertficats/ser5.jpg'
+ import Image6 from '../assets/image/sertficats/ser6.jpg'
 
 export default function Sertficat() {
     return (
