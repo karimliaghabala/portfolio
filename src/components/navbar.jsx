353261bda80d3ad265ea3c6773615b8546e45499
karimliaghabala <agabala.93@gmail.com>
2023-01-20 16:14:43 +0400

    update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,12 +5,12 @@ import '../components/navbar.css'
 export default function Navbar() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light nav-back ">
+    <nav className="navbar  navbar-expand-lg navbar-light bg-light nav-back ">
       <Link className="navbar-brand" to="/">Kərimli Ağabala</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <div className="collapse  navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
             <Link className="nav-link btn btn-outline-success my-2 my-sm-0 " to="/about">Haqqında <span className="sr-only">(current)</span></Link>
