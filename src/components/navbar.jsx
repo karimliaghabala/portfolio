a88ab5f5d02e98bfae97fb8a49d021751ac4d0ad
karimliaghabala <agabala.93@gmail.com>
2023-01-20 19:33:11 +0400

    update navbar logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import{ Link }from 'react-router-dom'
 import '../components/navbar.css'
+import LogoAga from '../assets/image/logoaga.jpg'
 
 export default function Navbar() {
   return (
     <>
     <nav className="navbar  navbar-expand-lg navbar-light  nav-back " style={{ backgroundColor: "rgba(19, 193, 236, 0.274)" }}>
-      <Link className="navbar-brand text-info" to="/">Kərimli Ağabala</Link>
+      <Link className="navbar-brand text-info logo" to="/"><img src={LogoAga} alt="aga" /></Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
