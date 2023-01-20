8f45df0348183b0fb23cfd35e649a990dd05bd07
karimliaghabala <agabala.93@gmail.com>
2023-01-21 00:18:23 +0400

    update

--- src/components/sertficat.jsx
+++ src/components/sertficat.jsx
@@ -10,41 +10,41 @@ import Image6 from '../assets/image/sertficats/udemysertf.jpg'
 export default function Sertficat() {
     return (
         <>
-        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-            <ol className="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div className="carousel-inner animate-box fadeInUp animated ">
-                <div className="carousel-item active">
-                    <img className="d-block w-100" src={Image1} alt="First slide"></img>
+            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div className="carousel-inner animate-box fadeInUp animated ">
+                    <div className="carousel-item active">
+                        <img className="d-block w-100" src={Image1} alt="First slide"></img>
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={Image2} alt="Second slide"></img>
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={Image3} alt="Third slide"></img>
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={Image4} alt="Fourth slide"></img>
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={Image5} alt="Fifth slide"></img>
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={Image6} alt="Sixth slide"></img>
+                    </div>
                 </div>
-                <div className="carousel-item">
-                    <img className="d-block w-100" src={Image2} alt="Second slide"></img>
-                </div>
-                <div className="carousel-item">
-                    <img className="d-block w-100" src={Image3} alt="Third slide"></img>
-                </div>
-                <div className="carousel-item">
-                    <img className="d-block w-100" src={Image4} alt="Fourth slide"></img>
-                </div>
-                <div className="carousel-item">
-                    <img className="d-block w-100" src={Image5} alt="Fifth slide"></img>
-                </div>
-                <div className="carousel-item">
-                    <img className="d-block w-100" src={Image6} alt="Sixth slide"></img>
-                </div>
+                <a className="carousel-control-prev slidePrev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="makePage">Geri</span>
+                </a>
+                <a className="carousel-control-next slideNext" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span className="makePage">İrəli</span>
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                </a>
             </div>
-            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
-            </a>
-            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
-            </a>
-        </div>
         </>
     )
 }
