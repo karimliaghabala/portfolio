4e2f4fd1af3be27c7b46d98cf08ab4b9cf81c650
karimliaghabala <agabala.93@gmail.com>
2023-01-20 16:23:39 +0400

    update

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -20,6 +20,9 @@ export default function About() {
     return (
         <>
             <div className="container res">
+            <div className="right">
+                    <img src={Myphoto} alt="myphoto" className='img-responsive animate-box fadeInUp animated' />
+                </div>
                 <div className="left fh5co-center-position ">
                     <p className='animate-box fadeInUp animated  '>03.03.1993 – cü ildə Zərdab rayonu Pərvanlı kəndində anadan olmuşam . 2011 – ci ildə 439 bal toplayaraq ADPU – nun Coğrafiya müəllimliyi ixtisasına qəbul olmuşam(ödənişsiz) . 2015 – ci ildə Akademik məzuniyyət götürərək Hərbi Xidmətə getmişəm. 2017 – ci ildə universiteti bitirib MİQ imtahanında 44 bal toplayaraq hazırda işlədiyim İmişli rayonu S.Məmmədov adına Axıcanlı kənd tam orta məktəbinə qəbul olmuşam. Hazırda əmək müqaviləmə əsasən həmin müəssisənin işçisiyəm. Əlavə olaraqda həmin rayonun A.Allahverdiyev adına Qaralar kənd tam orta məktəbdədə 3 il müddətli müqavilə ilə çalışmışam . Həmçinin hər iki məktəbin UTİS, ŞAGİRD-MƏZUN, e-gov və s. sistemlər üzrə kordinatoruyam.Artıq iş sahəmi İT - ə doğru dəyişmişəm</p>
                     {about? <p className='animate-box fadeInUp animated p2' >2017 – ci ildə bitirmişəm 2011 – ci ildə TB Bilgi tədris mərkəzində 6 aylıq Office proqramları, Adobe Photoshop proqramları üzrə kurs keçmişəm . Sertfikat verilib Təhsil sahəsində bir çox online kurslarda olmuşam. STEAM, UTİS və.s Hazırda 3 kursda iştirak edirəm . 1 – i əyani 2 – si isə online.
@@ -29,9 +32,7 @@ export default function About() {
                     <button className='btn btn-outline-success my-2 my-sm-0 animate-box fadeInUp animated' onClick={clickButton}>{text}</button>
                 </div>
 
-                <div className="right">
-                    <img src={Myphoto} alt="myphoto" className='img-responsive animate-box fadeInUp animated' />
-                </div>
+
             </div>
         </>
     )
