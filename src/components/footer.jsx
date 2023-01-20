0bac8f1c19e37cf5a11a4e6d2fb45260492f0e29
karimliaghabala <agabala.93@gmail.com>
2023-01-20 18:35:20 +0400

    update footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-import{ Link }from 'react-router-dom'
 import '../components/footer.css'
 import Linkedin from '../assets/image/linkedin.jpg'
+import Facebook from '../assets/image/facebook.jpg'
+import Twitter from '../assets/image/twitter.jpg'
+import Instagram from '../assets/image/instagram.jpg'
+import Whatsapp from '../assets/image/whatsapp.jpg'
 
 
 export default function Footer() {
@@ -9,14 +12,20 @@ export default function Footer() {
     <>
       <footer className="text-center text-white txtc" >
         {/* Grid container */}
-        <div className="container p-4">
-          <Link to="https://www.linkedin.com/in/agabala-karimli-317838a9/" target="_blank"> <img src={Linkedin} alt="linkedin" /></Link>
+        <div className="container container-1">
+          <div className='container-2'>
+          <a href="https://www.linkedin.com/in/agabala-karimli-317838a9/" target="_blank"> <img src={Linkedin} alt="linkedin" /></a>
+          <a href="https://www.facebook.com/heyatin.bashlangici/" target="_blank"> <img src={Facebook} alt="Facebook" /></a>
+          <a href="https://twitter.com/Kerimli_Agabala" target="_blank"> <img src={Twitter} alt="Twitter" /></a>
+          <a href="https://www.instagram.com/agabala_zerdabi/" target="_blank"> <img src={Instagram} alt="Instagram" /></a>
+          <a href="https://wa.me/qr/MAMPEMBTIOGOO1 " target="_blank"> <img src={Whatsapp} alt="Whatsapp" /></a>
+          </div>
         </div>
         {/* Grid container */}
         {/* Copyright */}
         <div className="text-center p-3 textFooter" >
           © 2023 Copyright:
-          <Link className="text-white" to="#!">Karimli Aghabala</Link>
+          <a className="text-white" href="#!">Karimli Aghabala</a>
         </div>
         {/* Copyright */}
       </footer>
