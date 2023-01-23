232ccf83f220dd5b1c39e3a0c7de730ad94998c2
karimliaghabala <agabala.93@gmail.com>
2023-01-24 00:22:58 +0400

    update contact
     irad və təklifləri göndərmək funksiyası əlavə
     olundu

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import './contact.css';
 import {useTranslation } from 'react-i18next';
 import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+    const form = useRef();
     const { t } = useTranslation();
     const handleClick = (e) => {
         e.preventDefault()
-        const serviceID = 'default_service';
-        const templateID = 'template_trcpd9k';
 
-        emailjs.sendForm(serviceID, templateID)
-            .then(() => {
-                alert('Məktub Göndərildi');
-            }, (err) => {
-                alert(JSON.stringify(err));
-            });
+    emailjs.sendForm('default_service', 'template_trcpd9k', form.current, 'Z7DQCa4EOTN-AkwnG')
+      .then((result) => {
+          console.log(result.text);
+          alert('Məktub göndərildi...')
+      }, (error) => {
+          console.log(error.text);
+      });
     };
     return (
         <div className='contact animate-box fadeInUp animated ' >
-            <form id="formFeedback ">
+            <form id="formFeedback " ref={form}>
                 <div className="form-group">
                     <label htmlFor="exampleFormControlInput1" className='text-white'>{t("fullname")}</label>
                     <input type="text" className="form-control" name="name" id="exampleFormControlInput1" required
