7822b254dddb944be51040b3c61afa4c5827b19d
Karimli Aghabala <54455724+karimliaghabala@users.noreply.github.com>
2023-02-02 00:03:29 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 import About from './components/about/about'
-import Contact from './components/contact/contact'
-import Myworks from './components/myworks/myworks'
-import Sertficat from './components/sertficat/sertficat'
-import Footer from './components/footer/footer'
-import Navbar from './components/navbar/navbar'
-import Seminar from './components/seminar/seminar'
-import News from './components/news/news'
-import { Routes, Route } from 'react-router-dom'
-import "./App.css"
-import "./components/css_library/animation.css"
+import Contact from './components/contact/contact';
+import Myworks from './components/myworks/myworks';
+import Sertficat from './components/sertficat/sertficat';
+import Footer from './components/footer/footer';
+import Navbar from './components/navbar/navbar';
+import Seminar from './components/seminar/seminar';
+import News from './components/news/news';
+import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import "./components/css_library/animation.css";
 export default function App() {
   return (
     <>
