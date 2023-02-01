4efc7726fc8d08a4c3bb1fc83c6853d7054988fd
Karimli Aghabala <54455724+karimliaghabala@users.noreply.github.com>
2023-02-02 01:49:10 +0400

    add responsive design

--- src/components/news/news.jsx
+++ src/components/news/news.jsx
@@ -16,7 +16,7 @@ const News = () => {
 
                         <div className="right-c">
                             <p>LET,VAR,CONST dəyişənlərinin fərqəri </p>
-                            <button className='btn btnHover' onClick={()=>{setNews(true)}} >Davamını oxu</button>
+                            <button className='btn btnHover' onClick={() => { setNews(true) }} >Davamını oxu</button>
                         </div>
                     </div>
 
@@ -24,13 +24,13 @@ const News = () => {
 
                 {news ? <div className="news" >
                     <h3>LET,VAR,CONST dəyişənlərinin fərqəri</h3>
-                    <p>"var", "const" ve "let" JavaScript'deki değişken tanımlama anahtar kelimeleridir ve farklı özelliklere sahiptirler:
+                    <p><p>"var", "const" ve "let" JavaScript'deki değişken tanımlama anahtar kelimeleridir ve farklı özelliklere sahiptirler:</p>
                         <img src={News1} alt="" />
                         <p>var: Eski tarz bir değişken tanımlama yöntemidir ve kapsam (scope) kuralları açısından sınırlıdır. Aynı isimde bir başka değişken tanımlanabileceği için tanımlama yerine göre farklı değerler alabilir ve global olarak erişilebilir.</p>
                         <img src={News2} alt="" />
                         <p>let: var'dan daha modern bir değişken tanımlama yöntemidir ve block scope kavramına sahiptir. Yani, tanımlandığı blok içinde geçerlidir ve dışında erişilemez. Aynı isimde başka bir değişken tanımlanamaz.</p>
                         <p>const: Değerinin değiştirilmemesi gereken değişkenler için kullanılır. Block scope kavramına sahiptir ve aynı isimde başka bir değişken tanımlanamaz. Değeri tanımlandıktan sonra değiştirilemez.</p></p>
-                        <button className='btn btnHover' onClick={()=>{setNews(false)}} >Əsas səhifəyə qayıt</button>
+                    <button className='btn btnHover' onClick={() => { setNews(false) }} >Əsas səhifəyə qayıt</button>
                 </div> : null}
 
             </div>
