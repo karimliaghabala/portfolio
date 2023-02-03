aee69cc52f7c26583033b4f91b37a5c27b451d26
Karimli Aghabala <54455724+karimliaghabala@users.noreply.github.com>
2023-02-04 00:57:32 +0400

    Update blog3.jsx

--- src/components/news/blog3.jsx
+++ src/components/news/blog3.jsx
@@ -16,7 +16,7 @@ const Blog2 = () => {
 İlk olaraq ‘Callback’ funksiyasına aid bir misal göstəcəm və izahı onun üzərindən verəcəm .</p>
     <img src={News1}  alt="" />
     <h6>Şəkil-1</h6>
-    <p>Sol hissədə yazdığım kod blokuna fikir versəz görəcəksiz ki, biz buttona click etdiyimiz zaman ilk olaraq setTimeout() funksiyasının çölündəki kod işə düşərək success yazısını ekrana yazdırdı , sonra isə setTimeout() funksiyası işə düşdü və 2 saniyyə sonra ikinci success yazısını ekrana yazdırdı .</p>
+    <p>Yuxarı hissədə yazdığım kod blokuna fikir versəz görəcəksiz ki, biz buttona click etdiyimiz zaman ilk olaraq setTimeout() funksiyasının çölündəki kod işə düşərək success yazısını ekrana yazdırdı , sonra isə setTimeout() funksiyası işə düşdü və 2 saniyyə sonra ikinci success yazısını ekrana yazdırdı .</p>
     <img src={News2} alt="" />
     <h6>Şəkil-2</h6>
     <p>Yəni bir birini gözləmədən işə düşdülər .Biz bu prosesə asinxron proqramlama deyirik. Asinxron proqramlamada kodlar bir birini gözləmədən işə düşür əgər bir birini gözləyərək işə düşən kod bloklarımız olsaydı browser donaraq heç bir proses həyata keçirməyəcəkdi.Bu asinxron proqramlamanın məntiqini anlamaq üçün yazdığım ən sadə nümunə idi. İndi başqa bir nümunə ilə daha da ətraflı izah verməyə çalışacam.</p>
@@ -26,7 +26,7 @@ const Blog2 = () => {
     <p>Gördüyünüz kimi data adı altında bir arrayimiz var və getArray funksiyasının köməyi ilə biz həmin arrayin elementlərini ala bilirik. Və daha sonra ora yeni bir data əlavə etmək istədiyimiz zaman yeni bir funksiya yaradaraq ora yeni datanı push etsək hərşey normal olaraq işləyəcək .Amma push etdiyimiz data müəyyən zaman intervalından sonra gəlsə işə düşməyəcəkdi. Çünki ilk olaraq getArray() funksiyasi çağrıldığı üçün newData() funksiyasi işə düşməyəcəkdi. Elə bu səbəbdən Callback funkisyalar köməyimizə çatır.</p>
     <img src={News4} alt="" />
     <h6>Şəkil-4</h6>
-    <p>Sol hissədə qeyd etdiyim kodlara nəzər salsaz görəcəksiniz ki ,Callback ilə push edəcəyimiz funksiyanın içərisinə bir funskiya daha yazırıq və onu parametr olaraq çağırırıq . Daha sonra isə ümumi datalarımızı çağırdığımız funksiyanı parametr olaraq yazırıq və istədiyimiz nəticəni alırıq</p>
+    <p>Yuxarı hissədə qeyd etdiyim kodlara nəzər salsaz görəcəksiniz ki ,Callback ilə push edəcəyimiz funksiyanın içərisinə bir funskiya daha yazırıq və onu parametr olaraq çağırırıq . Daha sonra isə ümumi datalarımızı çağırdığımız funksiyanı parametr olaraq yazırıq və istədiyimiz nəticəni alırıq</p>
 
     <img src={News5} alt="" />
     <h6>Şəkil-5</h6>
