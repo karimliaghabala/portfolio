58cd607587b66996ab0a2bcc68fd83eb6d4c20c9
karimliaghabala <agabala.93@gmail.com>
2023-09-30 18:25:44 +0400

    update

--- src/components/news/blog1.jsx
+++ src/components/news/blog1.jsx
@@ -1,34 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import './news.css'
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import './news.css';
 
 const Blog1 = () => {
-  const [data, setData] = useState([])
-  const {id} = useParams()
+  const [data, setData] = useState({});
+  const { id } = useParams();
+  console.log("params", id);
+
   useEffect(() => {
-      fetch(`https://portfolio-api-six-rosy.vercel.app/${id}`)
-          .then(res => res.json())
-          .then(apiData => setData(apiData))
-  }, [id])
+    fetch(`https://portfolio-api-six-rosy.vercel.app/${id}`)
+      .then((res) => res.json())
+      .then((apiData) => {
+        setData(apiData);
+        console.log(apiData);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, [id]);
+
   return (
     <>
-    <ul>
-      {data?.map(item=>(
-              <li className="news"  key={item.id}>
-              <h3>{item.title}</h3>
-              <p><p>{item.content}</p>
-                <img src={item.linkimg1} alt="img" />
-                <p>{item.content1}</p>
-                <img src={item.linkimg2} alt="img" />
-                <p>{item.content2}</p></p>
-                <Link className="nav-link btn text-white btnHover" to="/news">Əsas səhifəyə qayıt</Link>
-            </li>
-      ))}
-    </ul>
+      <div className="news" key={data.id}>
+        <h3>{data.title}</h3>
+        <p>
+          {data.content}
+          <img src={data.linkimg1} alt="img" /><br />
+          {data.content1} <br />
+          <img src={data.linkimg2} alt="img" /><br />
+          {data.content2}
+        </p>
+        <Link className="nav-link btn text-white btnHover" to="/news">Əsas səhifəyə qayıt</Link>
+      </div>
     </>
+  );
+};
 
-  )
-}
-
-export default Blog1
+export default Blog1;
